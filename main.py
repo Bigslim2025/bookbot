2bc5056a9402bbd918115ed3ec2c777648c37b33
Bigslim2025 <email@example.com>
2025-10-07 20:47:15 +0100

    added function to print the number of words in the text

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+def main():
+# Function defined to read the content of a book
+    text = get_book_text("./books/frankenstein.txt") # variable filled with the text read above
+    # print(text) # command to print the text
+    get_num_words(text) # call the function
 
 def get_book_text(book_path): 
 # Function defined to read the content of a book
     with open(book_path,'r', encoding="utf-8") as file_contents:
         return file_contents.read()
 
-def main():
-# variable filled with the text read above
-    text = get_book_text("./books/frankenstein.txt") 
-    print(text) # command to print the text
+def get_num_words(text):
+# Function defined count the words in a book
+    num_words = None
+    words_list = text.split() # split text into a list
+    num_words = len(words_list) # get through the list and find the number of words
+    print(f"Found {num_words} total words")
 
-main()
+if __name__ == "__main__":
+    main()
