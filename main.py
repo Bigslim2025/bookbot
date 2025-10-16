c6ac8f2d24ac65a26d694ffc2aa036dadbee06be
Bigslim2025 <email@example.com>
2025-10-16 18:44:56 +0100

    added new function to stats.py for counting the character in a book and printing it as a dictionary

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from stats import get_num_words
-from reader import get_book_text
+from stats import get_num_chars
 from library import frankenstein_text
 
-def main():
 
+def main():
+    # Function called to count the number of words in a book
     get_num_words(frankenstein_text)
+    get_num_chars(frankenstein_text)
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,6 +1,21 @@
 def get_num_words(frankenstein_text):
-# Function defined count the words in a book
+# Function defined to count the words in a book
     num_words = None
     words_list = frankenstein_text.split() # split text into a list
     num_words = len(words_list) # get through the list and find the number of words
     print(f"Found {num_words} total words")
+    return num_words
+
+def get_num_chars(frankenstein_text):
+# Function defined to count the characters in a book
+    frankenstein_text = frankenstein_text.lower() # converts the book to lower case to be read by the function
+    # print(frankenstein_text)
+    num_chars_dict = {}
+    for char in frankenstein_text:
+        if char in num_chars_dict:
+            num_chars_dict[char] += 1
+        else:
+            num_chars_dict[char] = 1
+    print(f"Found this number characters: {num_chars_dict}.")
+    return num_chars_dict
+    
