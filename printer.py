0830d2f2f140a7298166867ff1ef0fd746594249
Bigslim2025 <email@example.com>
2025-10-21 22:42:40 +0100

    created 'printer.py' for tidying up the code

---
+++ printer.py
@@ -0,0 +1,5 @@
+# File to be created
+
+# Needs to return the layout required by the assignment,
+# remove the "print" statements from their respective files 
+# and clear out the code for better coding experience.
